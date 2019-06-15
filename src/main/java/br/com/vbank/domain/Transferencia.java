c677e3c1dc91479686f18bfc3237f098bf277e98
frankcoutinho <frankcoutinho@outlook.com.br>
2019-06-15 15:16:22 -0300

    Alterado entidade Transferencia

--- src/main/java/br/com/vbank/domain/Transferencia.java
+++ src/main/java/br/com/vbank/domain/Transferencia.java
@@ -1,14 +1,17 @@
 package br.com.vbank.domain;
 
 import br.com.vbank.enums.SituacaoTransferencia;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "transferencia")
 public class Transferencia extends BaseEntity implements Serializable {
 	
@@ -21,21 +24,26 @@ public class Transferencia extends BaseEntity implements Serializable {
 	@Column(name = "vl_tranferencia", precision = 14, scale = 2, nullable = false)
 	private BigDecimal vlTransferencia;
 	
-	@Column(name = "dt_transferencia", nullable = true)
-	private LocalDate dtTransferencia;
-			
+	@Column(name = "dt_transferencia")
+	private LocalDateTime dtTransferencia;
+
+	@Column(name = "dt_agendamento", nullable = false)
+	private LocalDateTime dtAgendamento;
+
 	@Enumerated(EnumType.STRING)
 	@Column(name = "tp_situacao", length = 15, nullable = false)
 	private SituacaoTransferencia situacaoTransferencia;
-	
-	@JoinColumn(name = "dt_agendamento", referencedColumnName = "id", nullable = true)
-	private LocalDateTime dtAgendamento;
-	
+
 	public Transferencia() {
 
 	}
 	
-	public Transferencia(String nrContaRemetente, String nrContaFavorecido, BigDecimal vlTransferencia, LocalDate dtTransferencia) {
+	public Transferencia(
+		String nrContaRemetente,
+		String nrContaFavorecido,
+		BigDecimal vlTransferencia,
+		LocalDateTime dtTransferencia
+	) {
 		this.nrContaRemetente = nrContaRemetente;
 		this.nrContaFavorecido = nrContaFavorecido;
 		this.vlTransferencia = vlTransferencia;
@@ -43,53 +51,4 @@ public class Transferencia extends BaseEntity implements Serializable {
 		this.situacaoTransferencia = SituacaoTransferencia.PENDENTE;
 		this.dtAgendamento = LocalDateTime.now();
 	}
-
-	public String getNrContaRemetente() {
-		return nrContaRemetente;
-	}
-
-	public void setNrContaRemetente(String nrContaRemetente) {
-		this.nrContaRemetente = nrContaRemetente;
-	}
-
-	public String getNrContaFavorecido() {
-		return nrContaFavorecido;
-	}
-
-	public void setNrContaFavorecido(String nrContaFavorecido) {
-		this.nrContaFavorecido = nrContaFavorecido;
-	}
-
-	public BigDecimal getVlTransferencia() {
-		return vlTransferencia;
-	}
-
-	public void setVlTransferencia(BigDecimal vlTransferencia) {
-		this.vlTransferencia = vlTransferencia;
-	}
-
-	public LocalDate getDtTransferencia() {
-		return dtTransferencia;
-	}
-
-	public void setDtTransferencia(LocalDate dtTransferencia) {
-		this.dtTransferencia = dtTransferencia;
-	}
-
-	public SituacaoTransferencia getSituacaoTransferencia() {
-		return situacaoTransferencia;
-	}
-
-	public void setSituacaoTransferencia(SituacaoTransferencia situacaoTransferencia) {
-		this.situacaoTransferencia = situacaoTransferencia;
-	}
-
-	public LocalDateTime getDtAgendamento() {
-		return dtAgendamento;
-	}
-
-	public void setDtAgendamento(LocalDateTime dtAgendamento) {
-		this.dtAgendamento = dtAgendamento;
-	}
-
 }
